af2fa5d00792137d85ce13499a0dad4043ae1d1a
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2022-03-17 20:57:06 +0200

    Finished script

--- README.md
+++ README.md
@@ -9,7 +9,18 @@ Round robin for the tips: 1st, 2nd, ... last, 1st ...
 
 ## Setup instructions
 
-Clone the repo to your machine and ...
+Clone the repo to your machine
+
+Open your bash shell (Ubuntu, git bash...) and move to the directory where you placed the filed
 
 Make the script executable
 ````chmod +x linuxtips.sh````
+
+Place linuxtips.sh and the folder tips/ to /etc/profile.d/ directory
+Then the script will run on every login globally to all users
+````mv linuxtips.sh tips /etc/profile.d/````
+
+If you want the script to be user-specific, place it in
+````/home/$USER/.profile````
+
+At login you can cycle tips forward with "j" and press any other key to quit program
--- linuxtips.sh
+++ linuxtips.sh
@@ -2,14 +2,42 @@
 
 # Tool to display totd
 
+# Directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#echo "$SCRIPT_DIR"
+
+tip(){
+# Count number of tips in folder where script is
+NUMTIP=$(ls "$SCRIPT_DIR"/tips/*.txt | wc -l)
+echo "Number of tips: $NUMTIP"
+
 # Read the current tip number from curtip file
 NUM=$(<.curtip)
 
+# Check that current tip is in range and increment by one
+NUM=$(expr $NUM % $NUMTIP)
+NUM=$(expr $NUM + 1)
+echo Current: $NUM
+echo ""
+
 # Print out the content of the tip file
-cat $NUM.txt
+cat "$SCRIPT_DIR"/tips/$NUM.txt
+echo ""
 
 # Increment by one
-NUM=`expr $NUM + 1`
+# NUM=`expr $NUM + 1` different syntax
+#NUM=$(expr $NUM + 1)
 
 # Store the value to curtip file
 echo $NUM > .curtip
+
+# Prompt for more tips
+read -p "Show next tip? (j) " -n 1 -r answer
+if [ "$answer" = j ]; then
+	echo -e "\n"
+	tip
+fi
+}
+
+# call function
+tip
